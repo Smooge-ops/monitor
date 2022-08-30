601e888e9d8d4760ccf23c7872e957d10d9a896f
Smooge-ops <350445196@qq.com>
2022-08-30 17:10:47 +0800

    add cache

--- web/app.js
+++ web/app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 // 创建 express 的服务器实例
 const router = express();
+// 协商缓存
+const mcache = require("memory-cache");
 
 //修改限制大小
 var bodyParser = require("body-parser");
@@ -14,6 +16,29 @@ router.use(express.urlencoded({ extended: false }));
 // 一定要在路由之前，配置 cors 这个中间件，从而解决接口跨域的问题
 const cors = require("cors");
 router.use(cors());
+
+// 协商缓存
+app.use((req, res, next) => {
+	let key = "__express__" + req.originalUrl || req.url; //以接口路径作为key
+	let cachedBody = mcache.get(key);
+	//   检测key是否有对应的数据.如果有就直接返回
+	if (cachedBody) {
+		res.send(cachedBody);
+		console.log("cache");
+		// res.end("NOT Modified");
+		res.status(304);
+		return;
+	} else {
+		console.log("http");
+		res.sendResponse = res.send;
+		res.status(200);
+		res.send = body => {
+			mcache.put(key, body, 10 * 1000);
+			res.sendResponse(body);
+		};
+		next();
+	}
+});
 
 router.use(express.static("visual"));
 router.use(express.static("public"));
